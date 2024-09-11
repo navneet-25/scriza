0083c756a8c02c7090d631ab0328e4be41a325ce
Navneet <navneetpal718@gmail.com>
2024-09-12 02:32:00 +0530

    done

--- components/charts/LineAreaChart.tsx
+++ components/charts/LineAreaChart.tsx
@@ -1,7 +1,7 @@
-'use client';
-import dynamic from 'next/dynamic';
+"use client";
+import dynamic from "next/dynamic";
 // import Chart from 'react-apexcharts';
-const Chart = dynamic(() => import('react-apexcharts'), {
+const Chart = dynamic(() => import("react-apexcharts"), {
   ssr: false,
 });
 
@@ -9,7 +9,6 @@ const LineAreaChart = (props: any) => {
   const { chartData, chartOptions } = props;
 
   return (
-    // @ts-expect-error
     <Chart
       options={chartOptions}
       type="area"
